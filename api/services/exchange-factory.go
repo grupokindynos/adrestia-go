1a4935a35a20486f3417b16de312bd1f20cfcb6c
PrettyBoyHelios <helios@polispay.org>
2019-10-11 12:09:50 -0500

    Renames

--- api/services/exchange-factory.go
+++ api/services/exchange-factory.go
@@ -11,18 +11,19 @@ type ExchangeFactory struct {
 	Exchanges map[string]*exchanges.IExchange
 }
 
-var Exchanges = map[string]exchanges.IExchange{
+var ex = map[string]exchanges.IExchange{
 	"cryptobridge" : exchanges.CBInstance,
 	"binance" : exchanges.BinanceInstance,
 }
 
-func (e ExchangeFactory)GetExchangeByCoin(coin coins.Coin) (exchanges.IExchange, error) {
+func (e *ExchangeFactory)GetExchangeByCoin(coin coins.Coin) (exchanges.IExchange, error) {
 	var coinName = strings.ToLower(coin.Tag)
+
 	if coinName == "polis" || coinName == "xsg" || coinName == "colx"{
-		return Exchanges["cryptobridge"], nil
+		return ex["cryptobridge"], nil
 	}
 	if coinName == "dash" || coinName == "ltc" || coinName == "grs" || coinName == "xzc"{
-		return Exchanges["binance"], nil
+		return ex["binance"], nil
 	}
 	/*if coinName == "mnp" || coinName == "onion" || coinName == "colx"{
 		return exchanges.NewCrex()
@@ -30,5 +31,6 @@ func (e ExchangeFactory)GetExchangeByCoin(coin coins.Coin) (exchanges.IExchange,
 	/*if coinName == "btc" {
 		return exchanges.NewBitso()
 	}*/
+	
 	return *new(exchanges.Exchange), errors.New("exchange not found")
 }

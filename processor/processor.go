a33985cd472c7c20192e4a844d993effd3ee58c0
PrettyBoyHelios <helios@polispay.org>
2020-01-07 10:21:14 -0600

    Added processor steps

--- processor/processor.go
+++ processor/processor.go
@@ -15,6 +15,7 @@ func Start() {
 	adrestiaOrders, err := services.GetAllOrders(adrestia.OrderParams{
 		IncludeComplete: false,
 	})
+
 	if err != nil {
 		log.Fatal("Could not retrieve adrestiaOrders form Hestia", err)
 	}
@@ -35,12 +36,28 @@ func Start() {
 
 func handleCreatedOrders(wg *sync.WaitGroup) {
 	defer wg.Done()
-
 	fmt.Println("Finished CreatedOrders")
-
 }
+
 func handleSentAmount(wg *sync.WaitGroup) {
 	defer wg.Done()
-
+	// 1. Verifies deposit in exchange and creates Selling Order always targets BTC
 	fmt.Println("Finished SentAmount")
+}
+
+func handleFirstConversion(wg *sync.WaitGroup) {
+	defer wg.Done()
+	// 1. Checks if order has been fulfilled.
+	// 2. If target coin is BTC sends it to HW, else sends it to a second exchange
+	fmt.Println("Finished SentAmount")
+}
+
+func handleSecondExchange(wg *sync.WaitGroup) {
+	defer wg.Done()
+	// Verifies deposit in second exchange that targets the final coin. Arrives here if target is not BTC
+	fmt.Println("Finished SentAmount")
+}
+
+func handleConvertedCoins(wg *sync.WaitGroup) {
+	// Sends from final exchange to target coin HotWallet
 }
